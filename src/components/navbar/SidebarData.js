ac534f16f1cd19e41e309eb66fb8d70165ca7d75
Andjela937 <mmandic883@gmail.com>
2021-02-23 20:51:17 +0100

    laravel

---
+++ src/components/navbar/SidebarData.js
@@ -0,0 +1,135 @@
+const SidebarData = [
+  {
+    id: 0,
+    brand: "Mercedes-Benz S class",
+    iconClosed: "fas fa-caret-down",
+    iconOpened: "fas fa-caret-up",
+    subNav: [
+      {
+        id: 0,
+        brand: "Mercedes-Benz S class",
+        title: "S 350d",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika1.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika2.jpg",
+          "./img/Mercedes-Benz-slika3.jpg",
+          "./img/Mercedes-Benz-slika4.jpg",
+        ],
+      },
+      {
+        id: 1,
+        brand: "Mercedes-Benz S class",
+        title: "S 500",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika5.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika6.jpg",
+          "./img/Mercedes-Benz-slika7.jpg",
+          "./img/Mercedes-Benz-slika8.jpg",
+        ],
+      },
+      {
+        id: 2,
+        brand: "Mercedes-Benz S class",
+        title: "S 63 AMG",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika9.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika10.jpg",
+          "./img/Mercedes-Benz-slika11.jpg",
+          "./img/Mercedes-Benz-slika12.jpg",
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    brand: "Mercedes-Benz E class",
+    iconClosed: "fas fa-caret-down",
+    iconOpened: "fas fa-caret-up",
+    subNav: [
+      {
+        id: 0,
+        brand: "Mercedes-Benz E class",
+        title: "E 220d",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika13.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika14.jpg",
+          "./img/Mercedes-Benz-slika15.jpg",
+        ],
+      },
+      {
+        id: 1,
+        brand: "Mercedes-Benz E class",
+        title: "E Class Coupe",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika16.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika17.jpg",
+          "./img/Mercedes-Benz-slika18.jpg",
+          "./img/Mercedes-Benz-slika19.jpg",
+        ],
+      },
+      {
+        id: 2,
+        brand: "Mercedes-Benz E class",
+        title: "E 63 AMG",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika20.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika21.jpg",
+          "./img/Mercedes-Benz-slika22.jpg",
+          "./img/Mercedes-Benz-slika23.jpg",
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    brand: "Mercedes-Benz GLS",
+    iconClosed: "fas fa-caret-down",
+    iconOpened: "fas fa-caret-up",
+    subNav: [
+      {
+        id: 0,
+        brand: "Mercedes-Benz GLS",
+        title: "GLS 350d",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika24.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika25.jpg",
+          "./img/Mercedes-Benz-slika26.jpg",
+          "./img/Mercedes-Benz-slika27.jpg",
+        ],
+      },
+      {
+        id: 1,
+        brand: "Mercedes-Benz GLS",
+        title: "GLS 500d",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika28.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika29.jpg",
+          "./img/Mercedes-Benz-slika30.jpg",
+          "./img/Mercedes-Benz-slika31.jpg",
+        ],
+      },
+      {
+        id: 2,
+        brand: "Mercedes-Benz GLS",
+        title: "GLS 63 AMG",
+        path: "/car",
+        slika: "./img/Mercedes-Benz-slika32.jpg",
+        galerija: [
+          "./img/Mercedes-Benz-slika33.jpg",
+          "./img/Mercedes-Benz-slika34.jpg",
+          "./img/Mercedes-Benz-slika35.jpg",
+        ],
+      },
+    ],
+  },
+];
+
+export default SidebarData;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h2 className="home-title">Galerija</h2>
+      <p className="home-text">
+        -Mercedes-Benz Biševac-
+      </p>
+    </div>
+  );
+};
+
+export default Home;
